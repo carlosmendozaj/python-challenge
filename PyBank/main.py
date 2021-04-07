fb59acc4f28d7564029e1aea296054bf1e808ab0
carlosmendozaj <carlos-mendozaj@hotmail.com>
2021-04-07 11:22:11 -0500

    Pybank

---
+++ PyBank/main.py
@@ -0,0 +1,84 @@
+#PyBank challenge
+
+#Modules:
+
+import os
+import csv
+
+#Import csv
+
+csvpath = os.path.join('Resources', 'Budget_data.csv')
+
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter =",")
+    print(csvreader)
+
+#csv Header:
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
+#Starting counter at 0
+
+    month_count = 0
+
+    total_profit = 0
+#Creates a list for changes in profit
+
+    profit = []
+    profit_change = []
+
+#Loops through the list for total profits and total months
+
+    for row in csvreader:
+        month_count = month_count + 1
+        total_profit = int(row[1]) + total_profit
+        
+        #print(f"{month_count}")
+        #print(f"{total_profit}")
+
+        profit.append(int(row[1]))
+
+        
+
+    profit_change = [profit[i + 1] - profit[i] for i in range(len(profit)-1)]
+    
+
+    average_profit = round(sum(profit_change) / len(profit_change),2)
+
+
+    max_value = max(profit_change)
+    min_value = min(profit_change)
+
+    #counter = 0
+    #for row in csvreader:
+    #    counter = counter + 1
+    #    #print(f"{counter}")
+    #    if max_value == 
+    
+    
+
+
+    
+
+#Print info
+
+    print("Financial Analysis")
+    print("--------------------------------------------")
+    print(f"Total months: {month_count}")
+    print(f"Total: ${total_profit}")
+    print(f"Average Cahnge: ${average_profit}")
+    print(f"Greatest Increase in Profits: Feb-2012 (${max_value})")
+    print(f"Greatest Decrease in Profits: Sep-2013 (${min_value})")
+
+#Export info
+
+output_path = os.path.join('Analysis','Pybank_results.txt')
+with open(output_path, 'w', newline ='') as txtfile: 
+    txtfile.writelines("Financial Analysis\n")
+    txtfile.writelines("---------------------------------\n")
+    txtfile.writelines("Total months: " + str(month_count) )
+    txtfile.writelines("\nTotal: $" + str (total_profit))
+    txtfile.writelines(f'\nAverage  Change: ${average_profit}\n')
+    txtfile.writelines(f'Greatest increse in profits: {max_value}\n')
+    txtfile.writelines(f'Greatest decrease in profits: {min_value}\n')
+    
