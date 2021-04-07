ff4ac72fef265a09ca218547bfead6cdbdaefdb9
carlosmendozaj <carlos-mendozaj@hotmail.com>
2021-04-07 14:20:42 -0500

    Save

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -18,10 +18,25 @@ with open(csvpath) as csvfile:
 
     vote_count = 0
 
+
+    khan_votes = 0
+    correy_votes = 0
+    li_votes = 0
+    otooley_votes = 0
+
+    for  row in csvreader:
+        if str(row[2]) = str("Khan")
+            khan_votes = khan_votes + 1
+            
+    print(f"{khan_votes}")
+
     for row in csvreader:
         vote_count = vote_count + 1
 
         #print(f"{vote_count}")
 
-    print(f"{vote_count}")
+    print("Election Results")
+    print("------------------------------")
+    print(f"Total votes: {vote_count}")
+    print("------------------------------")
 
